993e31532d584d41eb3e05b12544124c5acb8f20
Fondati-Pietro <fondati.pietro@itismeucci.com>
2024-12-13 10:46:17 +0100

    errore risolto in stampa

--- stampa.cpp
+++ stampa.cpp
@@ -4,16 +4,18 @@ Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "
 Pietro Fondati 13/12/2024
 */
 
-# include <iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-  int i=10 ,n;
-  cout<<"Quante volte voi stampare la frase? ";
-  cin>>n;
-  while (i<n)
+  int i = 10, n;
+  cout << "Quante volte vuoi stampare la frase? ";
+  cin >> n;
+  while (i < n)
   {
-   cout<<"Bada come la fuma"<<endl; 
+    cout << "Bada come la fuma" << endl;
+    i++;
   }
   system("pause");
 }
